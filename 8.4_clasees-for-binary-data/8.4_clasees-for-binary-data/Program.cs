545861404c359d21193dde190ef97fa1722747d1
Dmitry Lazarenko <dobrobeam@gmail.com>
2022-05-07 00:05:02 +0300

    8.4 сериализация

---
+++ 8.4_clasees-for-binary-data/8.4_clasees-for-binary-data/Program.cs
@@ -0,0 +1,52 @@
+﻿class BinaryExperiment
+{
+    const string path = @"C:\Users\Dmitry Lazarenko\Desktop\BinaryFile.bin";
+
+    static void Main()
+    {
+        
+        // Считываем
+        ReadValues();
+        // Пишем
+        WriteValues();
+        // Снова считытваем
+        ReadValues();
+    }
+
+    static void WriteValues()
+    {
+        // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
+        using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append)))
+        {
+            // записываем данные в строковом формате
+            writer.Write($"Файл изменен {DateTime.Now} на компьютере Windows 10\n");
+        }
+    }
+
+    static void ReadValues()
+    {
+        string StringValue;
+        
+        if (File.Exists(path))
+        {
+            Console.WriteLine("Из файла считано:");
+            // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                // Применяем специализированные методы Read для считывания соответствующего типа данных.
+                try
+                {
+                    while ((StringValue = reader.ReadString()) != null)
+                    {
+                        Console.WriteLine("Строка: " + StringValue);
+                    }
+                    
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Чтение завершено: {e.Message}");
+                }      
+            }
+        }
+    }
+}
---
+++ 8.4_clasees-for-binary-data/8.4_serializable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+// объект для сериализации
+var person = new Pet("Rex", 2);
+Console.WriteLine("Объект создан");
+// создаем объект куда будем помещать объект для сериализации/десериализации
+BinaryFormatter formatter = new BinaryFormatter();
+
+// получаем поток, куда будем записывать сериализованный объект
+using (var fs = new FileStream("myPets.dat", FileMode.OpenOrCreate))
+{
+    // указываем объекту formatter куда и что будем сериализовывать
+    formatter.Serialize(fs, person);
+    Console.WriteLine("Объект сериализован");
+}
+// десериализация
+using (var fs = new FileStream("myPets.dat", FileMode.OpenOrCreate))
+{
+    //десириализуем из файла данные в виде объекта Pet
+    var newPet = (Pet)formatter.Deserialize(fs);
+    Console.WriteLine("Объект десериализован");
+    Console.WriteLine($"Имя: {newPet.Name} --- Возраст: {newPet.Age}");
+}
+Console.ReadLine();
+
+
+[Serializable]
+class Pet
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Pet(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
