5765ca3ae37bc4c96e436c4f75a15ba328600a70
Dmitry Lazarenko <dobrobeam@gmail.com>
2022-05-06 12:55:42 +0300

    8.3.1

---
+++ 8.2_classes-for-work-with-drive/8.3_classes-for-work-with-files/Program.cs
@@ -0,0 +1,41 @@
+﻿// Допустим, нам нужно создать файл и записать в него информацию, в коде выполним следующие действия: 
+// * Проверим существование файла
+// * Если не существует, создадим его и запишем строку.
+// * Откроем файл и прочитаем ранее записанную строку.
+
+string filePath = @"C:\test\text.txt"; // Укажем путь 
+
+if (!File.Exists(filePath)) // Проверим, существует ли файл по данному пути
+{
+    //   Если не существует - создаём и записываем в строку
+    using (StreamWriter sw = File.CreateText(filePath))  // Конструкция Using (будет рассмотрена в последующих юнитах)
+    {
+        sw.WriteLine("Олег");
+        sw.WriteLine("Дмитрий");
+        sw.WriteLine("Иван");
+    }
+}
+// Откроем файл и прочитаем его содержимое
+using (StreamReader sr = File.OpenText(filePath))
+{
+    string str = "";
+    while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+    {
+        Console.WriteLine(str);
+    }
+}
+
+//Задание 8.3.1
+//Исходный код программы — ещё один отличный пример текстового файла. 
+//Напишите программу, которая выводит свой собственный исходный код в консоль.
+
+string codePath = @"C:\Users\Dmitry Lazarenko\source\repos\module-8\8.2_classes-for-work-with-drive\8.3_classes-for-work-with-files\Program.cs";
+
+using (StreamReader sr = File.OpenText(codePath))
+{
+    string str = "";
+    while ((str = sr.ReadLine()) != null)
+    {
+        Console.WriteLine(str);
+    }
+}
---
+++ SharedProject1/SharedProject1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WMPLib;
+
+namespace SharedProject1
+{
+    internal class Class1
+    {
+    }
+}
