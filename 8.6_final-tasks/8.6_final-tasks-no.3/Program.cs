7afa1edf03a75ad2099d07ff95a1f8c72a0d9d82
Dmitry Lazarenko <dobrobeam@gmail.com>
2022-05-08 17:33:49 +0300

    8.6.3 готово, проверено

---
+++ 8.6_final-tasks/8.6_final-tasks-no.3/Program.cs
@@ -0,0 +1,133 @@
+﻿//ЗАДАНИЕ 3
+//Доработайте программу из задания 1, используя ваш метод из задания 2.
+
+//При запуске программа должна:
+// 1. Показать, сколько весит папка до очистки. Использовать метод из задания 2. 
+// 2. Выполнить очистку.
+// 3. Показать сколько файлов удалено и сколько места освобождено.
+// 4. Показать, сколько папка весит после очистки.
+
+string link = @"C:\test\";
+Folder folder = new Folder(link);
+folder.FolderClean30min(folder.path);
+
+
+public class Folder
+{
+    public string path;
+    public Folder(string path)
+    {
+        this.path = path;
+    }
+
+    // очищает файлы по заданному пути и в подпапках, не изменявшихся последние 30 минут
+    public void FolderClean30min(string path)
+    {
+        DirectoryInfo dir = new DirectoryInfo(path);
+        if (dir.Exists)
+        {
+            int deletedFilesNumber = 0;
+            long memoryCleaned = 0;
+            Console.WriteLine($"Папка: {dir.FullName}\t Размер до очистки: {GetLenght(dir.FullName)} байт");
+            // имеем ли доступ к директории?
+            try
+            {
+                // проверяем все файлы
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    if ((DateTime.Now - file.LastWriteTime) > TimeSpan.FromMinutes(30))
+                    {
+                        deletedFilesNumber++;
+                        memoryCleaned += file.Length;
+                        file.Delete();
+                        Console.WriteLine($"Файл: {file.FullName} - удален");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Файл: {file.FullName} - не удален");
+                        continue;
+                    }
+                }
+
+                // проверяем все подпапки и ищем в них файлы для очистки
+                foreach (DirectoryInfo directory in dir.GetDirectories())
+                {
+                    FolderClean30min(directory.FullName, ref deletedFilesNumber, ref memoryCleaned);                   
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Не удалось выполнить: {e.Message}");
+            }
+            Console.WriteLine($"Удалено: {deletedFilesNumber} файлов \t Памяти освобождено: {memoryCleaned} байт");
+            Console.WriteLine($"Папка: {dir.FullName}\t Размер после очистки: {GetLenght(dir.FullName)} байт");
+        }
+
+        else
+        {
+            Console.WriteLine($"Папка: {path}\t не существует");
+        }
+    }
+    // перегрузка метода для подсчета удаленных файлов и размера освобожденной памяти (рекурсивный)
+    public void FolderClean30min(string path, ref int deletedFilesNumber, ref long memoryCleaned)
+    {
+        DirectoryInfo subdir = new DirectoryInfo(path);
+        try
+        {
+            // проверяем все файлы
+            foreach (FileInfo file in subdir.GetFiles())
+            {
+                if ((DateTime.Now - file.LastWriteTime) > TimeSpan.FromMinutes(30))
+                {
+                    deletedFilesNumber++;
+                    memoryCleaned += file.Length;
+                    file.Delete();
+                    Console.WriteLine($"Файл: {file.FullName} - удален");
+                }
+                else
+                {
+                    Console.WriteLine($"Файл: {file.FullName} - не удален");
+                    continue;
+                }
+            }
+
+            // проверяем все подпапки и ищем в них файлы для очистки
+            foreach (DirectoryInfo directory in subdir.GetDirectories())
+            {
+                FolderClean30min(directory.FullName, ref deletedFilesNumber, ref memoryCleaned);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Не удалось выполнить: {e.Message}");
+        }
+    }
+    static long GetLenght(string path)
+    {
+
+
+        DirectoryInfo folder = new DirectoryInfo(path);
+
+        long sum = 0;
+        try
+        {
+            foreach (FileInfo file in folder.GetFiles())
+            {
+                sum += file.Length;
+            }
+            foreach (DirectoryInfo directory in folder.GetDirectories())
+            {
+                sum += GetLenght(directory.FullName);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Не удалось рассчитать: {e.Message}");
+        }
+
+        return sum;
+    }
+}
+
+
---
+++ 8.6_final-tasks/8.6_final-tasks/Program.cs
@@ -0,0 +1,93 @@
+﻿//ЗАДАНИЕ 1
+//Напишите программу, которая чистит нужную нам папку от файлов  и папок, которые не использовались более 30 минут 
+string link = @"C:\test\";
+//FolderClean(link);
+
+//ЗАДАНИЕ 2
+//Напишите программу, которая считает размер папки на диске (вместе со всеми вложенными папками и файлами). На вход метод принимает URL директории, в ответ — размер в байтах.
+DirectoryInfo dir = new DirectoryInfo(link);
+if (dir.Exists)
+{
+    Console.WriteLine($"Папка: {link}\t Размер: {GetLenght(link)} байт");
+}
+else
+{
+    Console.WriteLine($"Папка: {link}\t не существует");
+}
+
+
+
+// Задание 1
+static void FolderClean (string path)
+{
+    // имеем ли доступ к директории?
+    try
+    {
+        DirectoryInfo folder = new DirectoryInfo(path);
+        // существует ли данная директория?
+        if (folder.Exists)
+        {
+            // проверяем все файлы
+            foreach (FileInfo file in folder.GetFiles())
+            {
+                if ((DateTime.Now - file.LastWriteTime) > TimeSpan.FromMinutes(30))
+                {
+                    file.Delete();
+                    Console.WriteLine($"Файл: {file.FullName} - удален");
+                }
+                else
+                {
+                    Console.WriteLine($"Файл: {file.FullName} - не удален");
+                    continue;
+                }
+            }
+
+            // проверяем все папки
+            foreach (DirectoryInfo directory in folder.GetDirectories())
+            {
+                if ((DateTime.Now - directory.LastWriteTime) > TimeSpan.FromMinutes(30))
+                {
+                    directory.Delete(true);
+                    Console.WriteLine($"Директория: {directory.FullName} - удалена");
+                }
+                else
+                {
+                    Console.WriteLine($"Директория: {directory.FullName} - не удалена");
+                    continue;
+                }
+            }
+        }
+        else
+            Console.WriteLine("Директории по данной ссылке не существует");
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine($"Не удалось выполнить: {e.Message}");
+    }
+}
+// Задание 2
+static long GetLenght (string path)
+{
+    
+    
+    DirectoryInfo folder = new DirectoryInfo(path);
+
+    long sum = 0;
+    try
+    {
+        foreach (FileInfo file in folder.GetFiles())
+        {
+            sum += file.Length;
+        }
+        foreach(DirectoryInfo directory in folder.GetDirectories())
+        {
+            sum += GetLenght(directory.FullName);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Не удалось рассчитать: {e.Message}");
+    }
+    
+    return sum;
+}
